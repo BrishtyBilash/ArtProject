0ec67826ce30e3c67afe5f0ecbfe009957587205
BrishtyBilash <67582453+BrishtyBilash@users.noreply.github.com>
2020-09-20 21:46:54 +0600

    Blog created

--- ArtProject/urls.py
+++ ArtProject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Blog import views as Blog_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('showBlogs/', Blog_views.showBlog),
+    path('InsertBlogs/', Blog_views.insertBlog)
+
 ]
--- Blog/models.py
+++ Blog/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100, default="")
     bloggers_name = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    email = models.ForeignKey(Users, on_delete=models.CASCADE)
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together(('email', 'blog_id'))
